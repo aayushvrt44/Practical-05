ff828aca783d6a161de3f51304b41c3bfe8c74fa
aayushvrt44 <91362918+aayushvrt44@users.noreply.github.com>
2021-10-09 20:02:58 +0530

    Add files via upload

---
+++ TASK-01/TASK-01.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+ using namespace std;
+ 
+ int main ()
+ {
+       int n1 = 10               ;
+       float n2 = 10.5           ;
+       long n3 = 100             ;
+       short n4 = 10             ;
+       char n5 = 'A'             ;
+       bool n6 = 0               ;
+       double n7 = 3.14159       ;
+       long double n8 = 1.234567 ;
+       wchar_t n9 = L'A'         ;
+        
+       cout<<"The value in int variable is :"<< n1 << "\n"          ; 
+       cout<<"The value in float variable is :"<< n2 << "\n"        ; 
+       cout<<"The value in long variable is :"<< n3 << "\n"         ; 
+       cout<<"The value in short variable is :"<< n4 << "\n"        ; 
+       cout<<"The value in char variable is :"<< n5 << "\n"         ; 
+       cout<<"The value in bool variable is :"<< n6 << "\n"         ; 
+       cout<<"The value in double variable is :"<< n7 << "\n"       ; 
+       cout<<"The value in long double variable is :"<< n8 << "\n"  ; 
+       cout<<"The value in wide char variable is :"<< n9 << "\n"    ; 
+       
+       return 0 ;
+    }
